2a10bc680afe53494448600c4a09d581d6de6906
sdonne <stefanodonne@gmail.com>
2020-04-09 10:23:50 +0200

    Add sol

---
+++ log.sol
@@ -0,0 +1,68 @@
+Problem:    
+Rows:       12
+Columns:    32 (32 integer, 8 binary)
+Non-zeros:  64
+Status:     INTEGER OPTIMAL
+Objective:  obj = 4 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 cst_l_1                    -1                           0 
+     2 cst_l_2                    -1                           0 
+     3 cst_l_3                 -0.25                           0 
+     4 cst_l_4                     0                           0 
+     5 cst_l_5                     0                           0 
+     6 cst_l_6                     0                           0 
+     7 cst_l_7                     0                           0 
+     8 cst_l_8                 -0.61                           0 
+     9 cst_s_1                     4             4             = 
+    10 cst_s_2                     6             6             = 
+    11 cst_s_3                    12            12             = 
+    12 cst_cj                      4                           8 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 C_1          *              1             0             1 
+     2 C_2          *              1             0             1 
+     3 C_3          *              1             0             1 
+     4 C_4          *              0             0             1 
+     5 C_5          *              0             0             1 
+     6 C_6          *              0             0             1 
+     7 C_7          *              0             0             1 
+     8 C_8          *              1             0             1 
+     9 X_1_1        *              2             0               
+    10 X_2_1        *              0             0               
+    11 X_3_1        *              0             0               
+    12 X_1_2        *              2             0               
+    13 X_2_2        *              0             0               
+    14 X_3_2        *              0             0               
+    15 X_1_3        *              0             0               
+    16 X_2_3        *              5             0               
+    17 X_3_3        *              0             0               
+    18 X_1_4        *              0             0               
+    19 X_2_4        *              0             0               
+    20 X_3_4        *              0             0               
+    21 X_1_5        *              0             0               
+    22 X_2_5        *              0             0               
+    23 X_3_5        *              0             0               
+    24 X_1_6        *              0             0               
+    25 X_2_6        *              0             0               
+    26 X_3_6        *              0             0               
+    27 X_1_7        *              0             0               
+    28 X_2_7        *              0             0               
+    29 X_3_7        *              0             0               
+    30 X_1_8        *              0             0               
+    31 X_2_8        *              1             0               
+    32 X_3_8        *             12             0               
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
